6fa13b0f06201c19c1fe2bac007d5ef88c16ad25
juanmartinez <juanm.jm49@gmail.com>
2020-08-07 22:11:09 -0700

    added vineyard layout with its different vineyards

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, {useEffect, useState} from 'react';
 import logo from './grape_purpback.svg'
 import './App.css';
 import Login from "./components/Login.js";
+import Vineyards from "./components/Vineyards";
 
 // Login Page - Vineyard Manager
 function App() {
 
-	const [loggedOn, setLoggedOn] = useState(false);
+	const [isLoggedOn, setLoggedOn] = useState(false);
 	const [data, setData] = useState(null);
 
 	// Data
+	// add date field to sensor from vineyard object
 	const hook = () => {
 		console.log("Hook");
 		fetch("data.json")
@@ -28,8 +30,8 @@ function App() {
 			<header className="App-header">
 				<h1>Vineyard Manager</h1>
 				<img src={logo} className="App-logo" alt="logo" />
-				<Login logOn={loggedOn} setLogOn={setLoggedOn} uName={data ? data.companyName : null}/>
-
+				<Login logOn={isLoggedOn} setLogOn={setLoggedOn} uName={data ? data.companyName : null} />
+				{isLoggedOn ? <Vineyards vineyardData={data} /> : null}
 			</header>
 		</div>
 	);
--- src/components/Vineyards.js
+++ src/components/Vineyards.js
@@ -1,8 +1,33 @@
 import React, {useState} from 'react';
 import '../App.css';
 
-const Vineyards = (props) => {
+const VineyardBox = ({vyData}) => {
+    //console.log(vyData.vineyards[0].imgUrl);
+    return (
+        <div className="VineyardBox">
+            <div className="VineyardBox-Img">
+                <img src={vyData.imgUrl} alt={vyData.name + "Vineyard"}/>
+            </div>
+            <div className="VineyardBox-Text">
+                <p id='VineyardName'>{vyData.name}</p>
+                <p>T: {vyData.blocks[0].sensors[0].temperature}° | H: {vyData.blocks[0].sensors[0].humidity}%</p>
+                <p>{(new Date()).toLocaleDateString() + ' ' + (new Date()).toLocaleTimeString()}</p>
+            </div>
+        </div>
+    );
+}
 
+const Vineyards = ({vineyardData}) => {
+
+    const [vineyards, setVineyards] = useState(vineyardData.vineyards);
+
+    console.log(vineyards);
+
+    return (
+      <div className="Vineyards">
+          {vineyards.map((v, idx) => <VineyardBox key={v.name} vyData={v} />)}
+      </div>
+    );
 }
 
 export default Vineyards;
